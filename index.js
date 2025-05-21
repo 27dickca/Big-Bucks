b811b810cda68b9292859c132484e3e2d6375e25
27dickca <27dickca@students.lovelandschools.org>
2025-05-21 08:21:10 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let count = 0
+let countEl = document.getElementById("count-el")
+
+function increment1() {
+	count = count + 1
+	countEl.innerText = count
+	console.log(count)
+}
+function increment5() {
+	count = count + 5
+	countEl.innerText = count
+	console.log(count)
+}
+function increment10() {
+	count = count + 10
+	countEl.innerText = count
+	console.log(count)
+}
+function increment20() {
+	count = count + 20
+	countEl.innerText = count
+	console.log(count)
+}
+function increment50() {
+	count = count + 50
+	countEl.innerText = count
+	console.log(count)
+}
+function increment100() {
+	count = count + 100
+	countEl.innerText = count
+	console.log(count)
+}
